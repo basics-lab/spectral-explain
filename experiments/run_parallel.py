4480e691e132fd71dfdf90b134785f75d1febe85
aagarwal1996 <aa3797@berkeley.edu>
2025-01-18 19:05:45 +0000

    dynamic batching for faster experiments

--- experiments/run_parallel.py
+++ experiments/run_parallel.py
@@ -6,10 +6,10 @@ import os
 from collections import deque
 
 
-def run_job(job_id):
+def run_job(job_id, batch_size):
     device_id = job_id % torch.cuda.device_count()
     device = f'cuda:{device_id}'
-    command = f'python3 experiments/collect_data.py --seed {job_id} --device {device} --task {args.task} --MAX_B {args.MAX_B} --MAX_ORDER {args.MAX_ORDER} --num_test_samples {args.num_test_samples} --batch_size {args.batch_size} --verbose {args.verbose}'
+    command = f'python3 experiments/collect_data.py --seed {job_id} --device {device} --task {args.task} --MAX_B {args.MAX_B} --MAX_ORDER {args.MAX_ORDER} --num_test_samples {args.num_test_samples} --batch_size {batch_size} --verbose {args.verbose}'
     log_dir = f"experiments/logs/{args.task}"
     os.makedirs(log_dir, exist_ok=True)
     output_file = f"experiments/logs/{args.task}/job_{job_id}_output.txt"  # Output file for the job
@@ -21,51 +21,51 @@ def run_job(job_id):
     return result.returncode
 
 def process_completed_jobs(done, futures, job_ids):
-    for future, job_id in [(f, j) for f, j in futures if f in done]:
+    for future, job_id, batch_size in [(f, j, size) for f, j, size in futures if f in done]:
         try:
             result = future.result()  # Get the result (return code)
             if result != 0:  # If the job failed, re-add to the front
-                print(f"Job {job_id} failed. Re-adding to the back of the queue.")
-                job_ids.append(job_id)
+                print(f"Job {job_id} failed. Re-adding to the front of the queue.")
+                job_ids.append((job_id, batch_size//2))
             else:
                 print(f"Job {job_id} completed successfully with return code: {result}")
         except Exception as e:
             print(f"Job {job_id} raised an exception: {e}. Re-adding to the front of the queue.")
-            job_ids.appendleft(job_id)
+            job_ids.append((job_id, batch_size))
     # Update the futures list with only the jobs still running
-    return [(f, j) for f, j in futures if f not in done]    
+    return [(f, j, batch_size) for f, j, batch_size in futures if f not in done]    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--num_explain", type=int, default=20)
+    parser.add_argument("--num_explain", type=int, default=100)
     parser.add_argument("--seed", type=int, default=0)
     parser.add_argument("--task", type=str, default='drop')
     parser.add_argument("--MAX_B", type=int, default=8)
     parser.add_argument("--MIN_B", type=int, default=3)
     parser.add_argument("--MAX_ORDER", type=int, default=4)
     parser.add_argument("--num_test_samples", type=int, default=10000)
-    parser.add_argument("--batch_size", type=int, default=512)
+    parser.add_argument("--batch_size", type=int, default=1024)
     parser.add_argument("--verbose", type=bool, default=True)
     args = parser.parse_args()
     num_jobs = args.num_explain
     successfully_completed = 0
-    seeds = deque([i + args.seed for i in range(args.num_explain)])
+    job_ids = deque([(i + args.seed, args.batch_size) for i in range(args.num_explain)])
 
 
 
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=torch.cuda.device_count()) as executor:
+    with concurrent.futures.ProcessPoolExecutor(max_workers=torch.cuda.device_count()) as executor:
         futures = []
         
         # If the number of jobs is greater than the number of GPUs, wait for a GPU to finish
-        while seeds or futures:
+        while job_ids or futures:
             while len(futures) < torch.cuda.device_count():
-                job_id = seeds.popleft()
-                future = executor.submit(run_job, job_id)
-                futures.append((future, job_id))
+                job_id, batch_size = job_ids.popleft()
+                future = executor.submit(run_job, job_id, batch_size)
+                futures.append((future, job_id, batch_size))
             
             done, not_done = concurrent.futures.wait([f[0] for f in futures], return_when=concurrent.futures.FIRST_COMPLETED)
-            futures = process_completed_jobs(done, futures, seeds)
+            futures = process_completed_jobs(done, futures, job_ids)
             
 
 
